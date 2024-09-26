c44c7b2a388ad05a6ec31eb41b27801ecc4bda38
PatelAk282 <88264448+PatelAk282@users.noreply.github.com>
2024-09-26 23:34:35 +0000

    gemini-ai

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 
 # Manually set the API key in the script
-os.environ["GEMINI_API_KEY"] = "AIzaSyDasEWGvHbC8AINZAsNZOQBtgaAom17A2Q"  # Replace with your actual API key
+os.environ["GEMINI_API_KEY"] = ""  # Replace with your actual API key
 print(os.getenv("GEMINI_API_KEY"))  # This should now print the API key
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
